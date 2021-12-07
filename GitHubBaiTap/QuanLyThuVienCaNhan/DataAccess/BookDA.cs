3a7573d40bfdcdcc8988317ecc94245bec1d7549
Nguyễn Văn Nhựt <nguyenvannhut0704@gmail.com>
2021-12-07 15:24:50 +0700

    Loading project
    Loading

---
+++ GitHubBaiTap/QuanLyThuVienCaNhan/DataAccess/BookDA.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+using System.Data.SqlClient;
+using System.Threading.Tasks;
+
+namespace DataAccess
+{
+    public class BookDA
+    {
+        public List<Book> GetAll()
+        {
+            SqlConnection connection = new SqlConnection(Ultilities.ConnectionString);
+            connection.Open();
+            SqlCommand command = connection.CreateCommand();
+            command.CommandType = CommandType.StoredProcedure;
+            command.CommandText = Ultilities.Book_GetAll;
+            SqlDataReader reader = command.ExecuteReader();
+            List<Book> list = new List<Book>();
+            while (reader.Read())
+            {
+                Book book = new Book();
+                book.Ma = Convert.ToInt32(reader["MaSach"]);
+                book.Name = reader["TenSach"].ToString();
+                book.MaTheLoai = Convert.ToInt32(reader["MaTL"]);
+                book.TacGia = reader["TacGia"].ToString();
+                book.NhaXB = reader["NXB"].ToString();
+                book.NamXB = reader["NamXB"].ToString();
+                book.KeSach = Convert.ToInt32(reader["KeSach"]);
+                book.VTNgan = Convert.ToInt32(reader["VTNgan"]);
+                book.TrangThai = Convert.ToInt32(reader["TrangThai"]);
+                list.Add(book);
+            }
+            connection.Close();
+            return list;
+        }
+    }
+}
---
+++ GitHubBaiTap/QuanLyThuVienCaNhan/QuanLyThuVienCaNhan/Form1.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using BusinessLogic;
+using DataAccess;
+
+namespace QuanLyThuVienCaNhan
+{
+    public partial class Form1 : Form
+    {
+        List<Category> listCategory = new List<Category>();
+        List<Book> listBook = new List<Book>();
+        int bookID = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            LoadCategory();
+        }
+        private void CreatButton(int id, string name, int yPos)
+		{
+			var btn = new Button();
+			btn.Text = name;
+			btn.BackColor = Color.DarkOrchid;
+			btn.Height = 50;
+			btn.Width = 240;
+			btn.ForeColor = Color.White;
+			btn.Location = new Point(0, yPos);
+			DanhSachTheLoai.Controls.Add(btn);
+			btn.Tag = id;
+            btn.Click += CategoryButton_Click;
+		}
+
+        private void CategoryButton_Click(object sender, EventArgs e)
+        {
+            BookBL bookBL = new BookBL();
+            bookID = Convert.ToInt32((sender as Button).Tag);
+            listBook = bookBL.GetAll();
+            var books = listBook;
+            if (bookID > 0)
+            {
+                books = books.Where(f => f.MaTheLoai == bookID).ToList();
+            }
+            LoadFoodToLvDetail(books);
+        }
+        private void LoadFoodToLvDetail(List<Book> list)
+        {
+            lsvDanhSach.Items.Clear();
+            foreach (var book in list)
+            {
+                ListViewItem item = new ListViewItem(book.Ma.ToString());
+                item.SubItems.Add(book.Name);
+                item.SubItems.Add(book.MaTheLoai.ToString());
+                item.SubItems.Add(book.TacGia);
+                item.SubItems.Add(book.NhaXB);
+                item.SubItems.Add(book.NamXB);
+                item.SubItems.Add(book.KeSach.ToString());
+                item.SubItems.Add(book.VTNgan.ToString());
+                item.SubItems.Add(book.TrangThai.ToString());
+                lsvDanhSach.Items.Add(item);
+            }
+
+        }
+       
+        private void LoadCategory()
+		{
+			CategoryBL category = new CategoryBL();
+			listCategory = category.GetAll();
+			var yPos = 0;
+			CreatButton(0, "Tất cả", yPos);
+			foreach (var cat in listCategory)
+			{
+				yPos += 45;
+				CreatButton(cat.ID, cat.Name, yPos);
+			}
+		}
+      
+        private void tsmiMuonSach_Click(object sender, EventArgs e)
+        {
+            frmMuonTra frmMuon = new frmMuonTra();
+            frmMuon.ShowDialog();
+        }
+
+        private void lsvDanhSach_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            frmSach frmSach = new frmSach();
+            frmSach.ShowDialog();
+        }
+
+        private void tsmThongKe_Click(object sender, EventArgs e)
+        {
+            frmThongKe frmThongKe = new frmThongKe();
+            frmThongKe.ShowDialog();
+        }
+
+        private void tsmThem_Click(object sender, EventArgs e)
+        {
+            frmSach frmSach = new frmSach();
+            frmSach.ShowDialog();
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            //test test = new test();
+            //test.ShowDialog();
+        }
+    }
+}
